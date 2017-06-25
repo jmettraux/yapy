1f2afe894b7ed41341b02b1c376d2b78ef29dced
John Mettraux <jmettraux@gmail.com>
2017-06-26 06:22:33 +0900

    Make the default hello() a fourofour()

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ from wsgiref.simple_server import make_server
 #
 # route helpers
 
-def respond(env, start, content_type, body):
-    status = '200 OK'
+def respond(env, start, content_type, body, status='200 OK'):
+    status = status
     headers = [
       ('Content-Type', content_type),
       ('Content-Length', str(len(body))) ]
@@ -17,8 +17,8 @@ def respond(env, start, content_type, body):
 #
 # route implementations
 
-def hello(env, start):
-    return respond(env, start, 'text/plain', 'Hello.')
+def fourofour(env, start):
+    return respond(env, start, 'text/plain', 'Four O Four', '404 Not Found.')
 
 def quotes(env, start):
     params = parse_qs(env['QUERY_STRING'])
@@ -35,7 +35,7 @@ routes = {
 
 def application(environment, start_response):
     path0 = environment.get('PATH_INFO').split('/')[1]
-    route = routes.get(path0, hello)
+    route = routes.get(path0, fourofour)
     return route(environment, start_response)
 
 make_server('127.0.0.1', 8080, application).serve_forever()
