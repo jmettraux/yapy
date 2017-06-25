8a6e73a605b201f0c1e1d6c3f2e370658c217b8e
John Mettraux <jmettraux@gmail.com>
2017-06-26 05:55:12 +0900

    Implementing basic routing

--- server.py
+++ server.py
@@ -1,13 +1,32 @@
 
 from wsgiref.simple_server import make_server
 
-def application(environment, start_response):
-    #path = environment.get('PATH_INFO')
+#
+# route implementations
+
+def hello(env, start):
     res_body = "Hello."
     res_status = "200 OK"
     res_headers = [ ("Content-Length", str(len(res_body))) ]
-    start_response(res_status, res_headers)
+    start(res_status, res_headers)
     return [ res_body ]
+
+def quotes(env, start):
+    res_body = "QUOTES!"
+    res_status = "200 OK"
+    res_headers = [ ("Content-Length", str(len(res_body))) ]
+    start(res_status, res_headers)
+    return [ res_body ]
+
+#
+# application
+
+def application(environment, start_response):
+    path0 = environment.get('PATH_INFO').split('/')[1]
+    route = {
+      "quotes.js": quotes
+    }.get(path0, hello)
+    return route(environment, start_response)
 
 make_server('127.0.0.1', 8080, application).serve_forever()
 
