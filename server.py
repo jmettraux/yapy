fceaacff0be018276c184d56a3dbb6b4b561a10c
John Mettraux <jmettraux@gmail.com>
2017-06-27 05:54:24 +0900

    Fetch quotes from Yahoo

--- server.py
+++ server.py
@@ -1,7 +1,46 @@
 
 from cgi import parse_qs
+from urllib import quote
 from wsgiref.simple_server import make_server
+import requests
 
+
+#
+# "business" functions
+
+def fetch_yahoo_tickers(tickers):
+
+    tickers = \
+      ','.join(map((lambda t: '"' + t + '"'), tickers.split(',')))
+    query = \
+      'select * from yahoo.finance.quotes where symbol in (' + tickers + ')'
+    #print(query)
+    uri = \
+      'https://query.yahooapis.com/v1/public/yql' + \
+      '?q=' + quote(query) + \
+      '&format=json' + \
+      '&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+    #print(uri)
+
+    res = requests.get(uri)
+    if res.status_code != 200: return res.status_code
+
+    data = res.json()['query']
+    #print data['count']
+    print 'fetching ' + tickers + ', found ' + str(data['count']) + ' quotes'
+
+    quotes = data['results']['quote']
+    if data['count'] < 2: quotes = [ quotes ]
+
+    result = map(
+      lambda q: dict( \
+        symbol=q['Symbol'], \
+        currency=q['Currency'], \
+        price=float(q['LastTradePriceOnly'])),
+      quotes)
+    print result
+
+    return result
 
 #
 # route helpers
@@ -21,8 +60,9 @@ def fourofour(env, start):
 
 def quotes(env, start):
     params = parse_qs(env['QUERY_STRING'])
-    tickers = params.get('tickers', [ '' ])[0].split(',')
-    body = 'quotes for ' + ', '.join(tickers)
+    tickers = params.get('tickers', [ '' ])[0]
+    data = fetch_yahoo_tickers(tickers)
+    body = 'len: ' + str(len(data))
     return respond(env, start, 'text/plain', body)
 
 routes = {
